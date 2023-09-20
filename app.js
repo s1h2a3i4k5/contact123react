fb2ac8f955ee71146be3b055d0f3360943abd368
s1h2a3i4k5 <ahamedunnisah786@gmail.com>
2023-09-20 18:21:37 +0530

    save

---
+++ app.js
@@ -0,0 +1,9 @@
+const AppComponent = () =>{
+    return(
+        <div>
+            <h1>Welcome to contact App</h1>
+        </div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppComponent />);
